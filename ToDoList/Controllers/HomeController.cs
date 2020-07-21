127a51df0071e70117c9f030059e7efd81af6e1c
Noel Kirkland <noelkirkland@gmail.com>
2020-07-21 11:35:16 -0700

    add a foreach loop to display each list item
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
+
 {
   public class HomeController : Controller
   {
@@ -9,8 +11,8 @@ namespace ToDoList.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
     }
 
     [HttpGet("/items/new")]
@@ -23,7 +25,7 @@ namespace ToDoList.Controllers
     public ActionResult Create(string description)
     {
       Item myItem = new Item(description);
-      return View("Index", myItem);
+      return RedirectToAction("Index");
     }
     
   }
